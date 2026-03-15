Aureuix/HardLight

Shadekin updates, fixes, and additions (#862)
* ported shadekin system performance fix (Starlight PR #868)

* additional markings, eye color, sound effects, bug fixes (Starlight PRs #883 #899 #2381)

* Update ShadekinSystem.cs to remove nightvision call

* fixed lowercase A causing yml error :)

---------

Co-authored-by: fenndragon <fenndragon@gmail.com>

Resources/Locale/en-US/_Starlight/chat/emotes.ftl
@@ -11,7 +11,7 @@ chat-emote-name-bark = Bark
 chat-emote-name-yip = Yip
 chat-emote-name-whine = Whine
 chat-emote-name-pout = Pout
-chat-emote-name-mar = Mar
+chat-emote-name-marr = Marr
 chat-emote-name-wurble = Wurble
 chat-emote-name-scree = Scree
 chat-emote-name-call = Call
@@ -30,7 +30,7 @@ chat-emote-msg-bark = barks
 chat-emote-msg-yip = yips
 chat-emote-msg-whine = whines
 chat-emote-msg-pout = pouts
-chat-emote-msg-mar = marrs
+chat-emote-msg-marr = marrs
 chat-emote-msg-wurble = wurbles
 chat-emote-msg-scree = screes
 chat-emote-msg-call = calls

Triad-Sector/Triad

Resources/Locale/en-US/_Starlight/chat/emotes.ftl
@@ -9,6 +9,7 @@ chat-emote-name-growl = Growl
 chat-emote-name-bark = Bark
 chat-emote-name-yip = Yip
 chat-emote-name-whine = Whine
+chat-emote-name-pout = Pout
 chat-emote-name-mar = Mar
 chat-emote-name-wurble = Wurble
 chat-emote-name-scree = Scree
@@ -26,6 +27,7 @@ chat-emote-msg-growl = growls
 chat-emote-msg-bark = barks
 chat-emote-msg-yip = yips
 chat-emote-msg-whine = whines
+chat-emote-msg-pout = pouts
 chat-emote-msg-mar = marrs
 chat-emote-msg-wurble = wurbles
 chat-emote-msg-scree = screes